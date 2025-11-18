a60a9c6562df26c32268dc61178d0ba034e64919
Edgar <edgarmontenegro321@gmail.com>
2025-11-18 19:26:59 -0300

    ♻️ Revert changes

--- frontend/vet-client/src/services/supabase.js
+++ frontend/vet-client/src/services/supabase.js
@@ -1,6 +1,12 @@
-import { createClient } from '@supabase/supabase-js'
+import {createClient} from '@supabase/supabase-js'
+
+export const supabase = createClient('https://tpmqibdozwfragobenuk.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRwbXFpYmRvendmcmFnb2JlbnVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyODk1NzEsImV4cCI6MjA3Nzg2NTU3MX0.mz4cDLo-BDHHkIwO9f-8cfJuk3TegBCjDaut00V8Zw8')
+
+
+/*import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey)*/
