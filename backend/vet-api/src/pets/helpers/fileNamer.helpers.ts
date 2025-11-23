c20655e68c87e883126627b2b1b6a7d82513de4e
Leontwas <leonardo_dl@hotmail.com.ar>
2025-11-23 18:56:01 -0300

    I cleaned code

--- backend/vet-api/src/pets/helpers/fileNamer.helpers.ts
+++
@@ -1,9 +0,0 @@
-import { Request } from "express";
-import { v4 as uuid } from 'uuid'
-
-export const fileNamer = (req: Request, file: Express.Multer.File, callback: Function) => {
-
-    const fileExtension = file.mimetype.split('/')[1]
-
-    callback(null, `${uuid()}.${fileExtension}`)
-}
